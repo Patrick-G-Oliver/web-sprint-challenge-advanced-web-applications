5625b5e91e3f4b1df4748ff68018c3f4de9fe85c
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-09-13 19:46:19 -0400

    axiosWithAuth and token retrieval in place

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,65 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+// username = Lambda School, password = i<3Lambd4
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials, 
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  // make a post request to retrieve a token from the api
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("http://localhost:5000/api/login", this.state.credentials)
+      .then(res => {
+        console.log("loggin in", res)
+        localStorage.setItem("token", res.data.payload);
+         // when you have handled the token, navigate to the BubblePage route
+         this.props.history.push("/protected");
+      })
+      .catch(err => console.log({ err }));
+  }
+
+  render() {
+    return (
+      <div> 
+        <form onSubmit={this.login}>
+          <input 
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input 
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  };
+};
+
+export default Login;
+
+/* 
 
 const Login = () => {
   // make a post request to retrieve a token from the api
@@ -11,4 +72,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+*/
