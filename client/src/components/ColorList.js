8b2ad356faaccbd17de608cab97458f1c978a7bf
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-09-13 20:16:58 -0400

    edit and delete features functioning properly

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+const ColorList = ({ colors, updateColors, getColors }) => {
+  // console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,10 +21,23 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then( (res) => {
+        console.log(res.data);
+        editColor(colorToEdit);
+        getColors();
+      })
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then( (res) => {
+        console.log(res.data)
+        getColors();
+      })
   };
 
   return (
